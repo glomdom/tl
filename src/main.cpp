70deed68fb8b5371bdbe70c512a145f497e2dcc0
glomdom <103685817+glomdom@users.noreply.github.com>
2026-07-10 18:52:46 +0300

    feat: pretty print better

--- src/main.cpp
+++ src/main.cpp
@@ -39,36 +39,80 @@ namespace fs = std::filesystem;
 
 using namespace tlc::core;
 
-std::string visit_type_variant(const ast::types::Type& type) {
+std::string visit_type(const ast::types::Type& type) {
   return std::visit(overloaded{
-    [](const ast::types::NamedType& nt) -> std::string { return nt.name; }
+    [=](const ast::types::NamedType& nt) -> std::string {
+      return nt.name;
+    }
   }, type);
 }
 
-std::string visit_expression(const ast::expressions::Expression& expr) {
+std::string visit_expression(const ast::expressions::Expression& expr, const std::size_t depth) {
+  const auto padding = std::string(depth * 2, ' ');
+
   return std::visit(overloaded{
-    [](const ast::expressions::BinaryExpression& e) -> std::string { return std::format("BinaryExpression(left={} op={} right={})", visit_expression(*e.left), e.op, visit_expression(*e.right)); },
-    [](const ast::expressions::NumberExpression& e) -> std::string { return std::format("NumberExpression(value={})", e.value);},
-    [](const ast::expressions::IdentifierExpression& e) -> std::string { return std::format("IdentifierExpression(name={})", e.name); },
-    [](const ast::expressions::StringExpression& e) -> std::string { return std::format("StringExpression(value={})", e.value); },
-    [](const ast::expressions::CallExpression& e) -> std::string {
+    [=](const ast::expressions::BinaryExpression& e) -> std::string {
+      std::string result = std::format("{}BinaryExpression {}\n{}\n{}", padding, e.op, visit_expression(*e.left, depth + 1), visit_expression(*e.right, depth + 1));
+
+      return result;
+    },
+    [=](const ast::expressions::NumberExpression& e) -> std::string { return std::format("{}NumberExpression {}", padding, e.value);},
+    [=](const ast::expressions::IdentifierExpression& e) -> std::string { return std::format("{}IdentifierExpression {}", padding, e.name); },
+    [=](const ast::expressions::StringExpression& e) -> std::string { return std::format("{}StringExpression {}", padding, e.value); },
+    [=](const ast::expressions::CallExpression& e) -> std::string {
       std::string args = e.args
-        | std::views::transform([](const ast::expressions::ExpressionPointer& p) {
-          return std::format("Argument(value={})", visit_expression(*p));
+        | std::views::transform([=](const ast::expressions::ExpressionPointer& p) {
+          return std::format("{}", visit_expression(*p, depth + 1));
         })
         | std::views::join_with(std::string{", "})
         | std::ranges::to<std::string>();
 
-      return std::format("CallExpression(callee={} args={})", visit_expression(*e.callee), args);
+      return std::format("{}CallExpression\n{}\n{}", padding, visit_expression(*e.callee, depth + 1), args);
     },
   }, expr);
 }
 
-std::string visit_statement(const ast::statements::Statement& stmt) {
+std::string visit_statement(const ast::statements::Statement& stmt, const std::size_t depth) {
   return std::visit(overloaded{
-    [](const ast::statements::ReturnStatement& s) -> std::string { return std::format("Return(expr={})", visit_expression(*s.returned)); },
-    [](const ast::statements::ExpressionStatement& s) -> std::string { return visit_expression(*s.expression); },
+    [=](const ast::statements::ReturnStatement& s) -> std::string {
+      const auto padding = std::string(depth * 2, ' ');
+
+      std::string result = std::format("{}Return\n{}", padding, visit_expression(*s.returned, depth + 1));
+
+      return result;
+    },
+    [=](const ast::statements::ExpressionStatement& s) -> std::string { return visit_expression(*s.expression, depth); },
   }, stmt);
+}
+
+std::string visit_function_declaration(const ast::declarations::FunctionDeclaration& fn, const std::size_t depth) {
+  const auto padding = std::string(depth * 2, ' ');
+
+  std::string params = fn.parameters
+    | std::views::transform([=](const ast::declarations::Parameter& p) {
+      return std::format("  {}Parameter {}", padding, p.name);
+    })
+    | std::views::join_with(std::string{"\n"})
+    | std::ranges::to<std::string>();
+
+  std::string statements = fn.body.statements
+    | std::views::transform([=](const ast::statements::StatementPointer& s) {
+      return std::format("{}", visit_statement(*s, depth + 1));
+    })
+    | std::views::join_with(std::string{"\n"})
+    | std::ranges::to<std::string>();
+
+  std::string result = std::format("{0}Function {1}\n{0}{2}\n{0}{3}", padding, fn.name, params, statements);
+
+  return result;
+}
+
+std::string visit_declaration(const ast::declarations::Declaration& decl, const std::size_t depth) {
+  return std::visit(overloaded{
+    [=](const ast::declarations::FunctionDeclaration& fn) {
+      return visit_function_declaration(fn, depth);
+    },
+  }, decl);
 }
 
 int main(int argc, char* argv[]) {
@@ -99,27 +143,7 @@ int main(int argc, char* argv[]) {
   }
 
   for (const auto& decl : *declarations) {
-    std::visit(overloaded{
-      [](const ast::declarations::FunctionDeclaration& fn) {
-        std::string params = fn.parameters
-          | std::views::transform([](const ast::declarations::Parameter& p) {
-            return std::format("Parameter(name={} type={})", p.name, visit_type_variant(*p.type));
-          })
-          | std::views::join_with(std::string{", "})
-          | std::ranges::to<std::string>();
-
-        std::string statements = fn.body.statements
-          | std::views::transform([](const ast::statements::StatementPointer& s) {
-            return std::format("  {}", visit_statement(*s));
-          })
-          | std::views::join_with(std::string{"\n"})
-          | std::ranges::to<std::string>();
-
-        std::println("Function(name={} params={} returnType={}) {{", fn.name, params, visit_type_variant(*fn.returnType));
-        std::println("{}", statements);
-        std::println("}}");
-      },
-    }, decl);
+    std::println("{}", visit_declaration(decl, 0));
   }
 
   return 0;
